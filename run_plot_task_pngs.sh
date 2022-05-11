5f74c50d811a51005aedfe04bee3f69f8d0aef77
Maijer <smaijer@res-hpc-lo02.researchlumc.nl>
2022-05-11 10:43:32 +0200

    Iniit en task desc

--- run_plot_task_pngs.sh
+++ run_plot_task_pngs.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
-read -p "Default partition is LKEBgpu. If not desired, type other partition name:" p
-p=${p:-LKEBgpu}
-
 read -p "Default cpu amount is 6. If not desired, type other amount:" cpu
 cpu=${cpu:-6}
 
-read -p "Default wall time is 01:00:00. If not desired, type other wall time:" t
-t=${t:-01:00:00}
-
-read -p "Enter config [3d_lowres, 3d_cascade_fullres, 3d_fullres]:" config
+read -p "Default wall time is 00:10:00. If not desired, type other wall time:" t
+t=${t:-00:10:00}
 
 # We assume running this from the script directory
 job_directory=/home/smaijer/slurm/jobs/
-job_file="${job_directory}/inference_${p}_${cpu}_${config}_$(date +"%Y_%m_%d_%I_%M_%p").job"
+job_file="${job_directory}/plot_${cpu}_$(date +"%Y_%m_%d_%I_%M_%p").job"
 
 echo "#!/bin/bash
-#SBATCH -J NIHPancreasInference
-#SBATCH -p $p
+#SBATCH -J NIHPancreasPlot
 #SBATCH -N 1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=$cpu
 #SBATCH --time=$t
-#SBATCH --mem=32GB
-#SBATCH --gres=gpu:RTX6000:1
-#SBATCH --error=/home/smaijer/logs/inference/500/job.%J.err
-#SBATCH --output=/home/smaijer/logs/inference/500/job.%J.out
+#SBATCH --mem=12GB
+#SBATCH --error=/home/smaijer/logs/plot/500/job.%J.err
+#SBATCH --output=/home/smaijer/logs/plot/500/job.%J.out
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=susy.maijer@lumc.nl
 
@@ -57,7 +50,7 @@ conda env config vars list
 echo \"Installing nnU-net..\"
 pip install -e /home/smaijer/code/nnUNet
 
-nnUNet_predict -i $nnUNet_raw_data_base/nnUNet_raw_data/Task500_NIH_Pancreas/imagesTr -o $OUTPUT/500/$config/inference -t 500 -m $config
+nnUNet_plot_task_pngs -t 500 -o $OUTPUT/500/plot
 
 echo \"Program finished with exit code $? at: `\date`\"" > $job_file
 sbatch $job_file
